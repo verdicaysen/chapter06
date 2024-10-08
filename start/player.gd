a38d3b05c6ac229f734f789f7ee53a7899a7a336
r.ashley <r.ashley@talonsnest.games>
2024-10-07 19:43:08 -0500

    Completed level layout.

--- start/player.gd
+++ start/player.gd
@@ -7,16 +7,25 @@ const MAX_HEALTH: int = 10
 @onready var _health_label: Label = $HealthLabel
 
 # This is basically current health.
-var health: int = 10
+@export var health: int = 10:
+	get:
+		return health
+	set(new_value):
+		health = clamp(new_value, 0, MAX_HEALTH)
+		update_health_label()
 
 # Main scene tree function to get everything in the tree moving at game start.
 func _ready():
-	pass
-
-func add_health_points(difference: int):
-	health += difference
-	health = clamp(health, 0, MAX_HEALTH)
+	print(health)
 	update_health_label()
 
+# We are adding health points via a function, and clamping it to the maximum health constant, then updating it in the UI with the update health label function.
+func add_health_points(difference: int):
+	health += difference
+
+# This is a function to update the string in Health Label to the accurate number based on the variables.
 func update_health_label():
+	if not is_instance_valid(_health_label):
+		return
+		
 	_health_label.text = str(health) + "/" + str(MAX_HEALTH)
